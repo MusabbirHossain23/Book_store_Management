d94f1bf8b56bc7f2584f9e749c1104c1a071761f
MusabbirHossain23 <murad.bnmpc2351@gmail.com>
2023-10-01 15:06:28 +0600

    first commit

--- book_store/settings.py
+++ book_store/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 MEDIA_URL = '/media/'  # The URL prefix for serving media files
 MEDIA_ROOT = BASE_DIR / 'media'  # The local filesystem path where media files are stored
